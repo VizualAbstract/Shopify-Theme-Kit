9a682f8923d00c9d14142388f6a94b9845f23d4c
Corey Capetillo <corey.capetillo@gmail.com>
2015-06-16 10:08:02 -0700

    Retool as a Starter Kit for Shopify development

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      styles: {
+        files: ['source/styles/**/*.scss'],
+        tasks: ['concat:styles'],
+        options: {
+          interrupt: true,
+          spawn: false,
+        },
+      },
+      scripts: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint', 'concat:scripts'],
+        options: {
+          interrupt: true,
+          spawn: false,
+        },
+      },
+      images: {
+        files: ['source/images/**'],
+        tasks: ['imagemin'],
+        options: {
+          interrupt: true
+        },
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'source/javascript/**/*.js']
+    },
+    concat: {
+      options: {
+        seperator: ';',
+      },
+      scripts: {
+        src: ['source/**/*.js'],
+        dest: 'theme/assets/main.js.liquid',
+      },
+      styles: {
+        src: ['source/**/*.scss'],
+        dest: 'theme/assets/main.scss.liquid',
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'source/images/',
+          src: ['**/*.{jpg,gif,png}'],
+          dest: 'theme/assets/'
+        }],
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // grunt.loadNpmTasks('grunt-contrib-sass');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['jshint','concat','imagemin','watch']);
+};
