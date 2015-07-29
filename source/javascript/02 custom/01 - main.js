fa3db960425193d7757ea9aa7e9a7385d310ed72
Corey Capetillo <corey.capetillo@gmail.com>
2015-07-28 17:11:30 -0700

    Add variant image swaping and tie it to carousels for product image and thumbnails

--- source/javascript/02 custom/01 - main.js
+++ source/javascript/02 custom/01 - main.js
@@ -5,47 +5,56 @@
 ---------------------------------------- */
 $(function(){
 	var $carouselHome = $('.carousel');
-	$carouselHome.slick({
-		// 'autoplay': true,
-		// 'autoplaySpeed': 3000,
-		// 'centerMode': true,
-		// 'dots': true,
-		// 'draggable': true,
-		// 'fade': true,
-		// 'pauseOnHover': false,
-		// 'speed': 400,
-	});
-
-	function addThumbnails(){
-		// $navigation = $('.slick-dots');
-		// $('.slick-dots').destroy();
-		// $('.slick-initialized').append($navigation);
-	}
+	$carouselHome.slick();
 
 	var $carouselProduct = $('.product-gallery');
+	var $carouselThumbnails = $('.product-single__thumbnails');
 
-	// $carouselProduct.on('init', function(event, slick){
-	// 	addThumbnails();
-	// });
+	function thumbnailBrowser(){
+		$carouselThumbnails.on('click', 'a', function(e){
+			e.preventDefault();
+			var $el = $(this);
+			var $index = $el.data('index');
+			$carouselProduct.slick('slickGoTo', $index);
+		});
+	}
 
-	$carouselProduct.slick({
-		// 'autoplay': false,
-		// 'centerMode': true,
-		// 'dots': true,
-		// 'draggable': true,
-		// 'fade': true,
+	function variantSlide(){
+		var $thumbnailContainer = $('.product-single__thumbnails');
+		var $activeVariant = $('.single-option-selector').val();
+		var $target = $thumbnailContainer.find('a[data-variantName="' + $activeVariant +'"]:first');
+		var $index = $target.data('index');
+		return $index;
+	}
+
+	// When the product carousel is initialized, enable thumbnailBrowser
+	$carouselProduct.on('init', function(event, slick){
+		thumbnailBrowser();
 	});
 
-	var $carouselThumbnails = $('.product-thumbnails');
+	/* Initialize Product Gallery Carousel */
+	$carouselProduct.slick();
 
-	$carouselThumbnails.on('click', 'li a', function(e){
-		e.preventDefault();
-		var $el = $(this);
-		var $index = $el.data('index');
-		// var $image = $el.data('id');
-		// var $variant = $el.data('variant');
-		$carouselProduct.slick('slickGoTo', $index);
+	$carouselThumbnails.on('init', function(event, slick){
+		$carouselProduct.slick('slickGoTo', variantSlide());
+		// $carouselThumbnails.slick('slickGoTo', variantSlide());
+	});
+	/* Initialize Thumbnails Carousel */
+	$carouselThumbnails.slick({
+		slidesToShow: 4,
+		slidesToScroll: 3,
+		infinite: true,
+  	focusOnSelect: true
 	});
 
+	var $selector = $('.single-option-selector');
+	$selector.on('change', function(){
+		var $target = $('.product-single__thumbnails a[data-variantName="' + $(this).val() +'"]:first');
+		var $index = $target.data('index');
+		if ($index != undefined){
+			$carouselProduct.slick('slickGoTo', $index);
+			$carouselThumbnails.slick('slickGoTo', $index);
+		}
+	});
 
 });
