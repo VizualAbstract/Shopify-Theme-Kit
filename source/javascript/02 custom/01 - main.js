0de77b0e89800fa4f9cf71496d0925439e730eaa
Corey Capetillo <corey.capetillo@gmail.com>
2015-07-28 17:20:06 -0700

    Clean code and refactor

--- source/javascript/02 custom/01 - main.js
+++ source/javascript/02 custom/01 - main.js
@@ -7,39 +7,41 @@ $(function(){
 	var $carouselHome = $('.carousel');
 	$carouselHome.slick();
 
-	var $carouselProduct = $('.product-gallery');
-	var $carouselThumbnails = $('.product-single__thumbnails');
+	var $carouselProduct = $('.product-gallery'),
+			$carouselThumbnails = $('.product-single__thumbnails');
 
+	// Functions that will be run after the carousels are initialized */
+	/* Turns the thumbnails into a working carousel-navigator */
 	function thumbnailBrowser(){
+		var $carouselProduct = $('.product-gallery'),
+				$carouselThumbnails = $('.product-single__thumbnails');
 		$carouselThumbnails.on('click', 'a', function(e){
 			e.preventDefault();
 			var $el = $(this);
 			var $index = $el.data('index');
-			$carouselProduct.slick('slickGoTo', $index);
+			$carouselThumbnails.slick('slickGoTo', $index);
 		});
 	}
-
+	/* Returns the slide-index of a matching image to a selected variant */
 	function variantSlide(){
-		var $thumbnailContainer = $('.product-single__thumbnails');
-		var $activeVariant = $('.single-option-selector').val();
-		var $target = $thumbnailContainer.find('a[data-variantName="' + $activeVariant +'"]:first');
+		var $thumbnailContainer = $('.product-single__thumbnails'),
+				$activeVariant = $('.single-option-selector').val(),
+				$target = $thumbnailContainer.find('a[data-variantName="' + $activeVariant +'"]:first');
 		var $index = $target.data('index');
 		return $index;
 	}
 
-	// When the product carousel is initialized, enable thumbnailBrowser
+	// Runs functions when the carousels are initialized
 	$carouselProduct.on('init', function(event, slick){
 		thumbnailBrowser();
 	});
-
-	/* Initialize Product Gallery Carousel */
-	$carouselProduct.slick();
-
 	$carouselThumbnails.on('init', function(event, slick){
 		$carouselProduct.slick('slickGoTo', variantSlide());
-		// $carouselThumbnails.slick('slickGoTo', variantSlide());
+		/* $carouselThumbnails.slick('slickGoTo', variantSlide()); // Needs more work, as above isn't working */
 	});
-	/* Initialize Thumbnails Carousel */
+
+	// Triggers the carousels
+	$carouselProduct.slick();
 	$carouselThumbnails.slick({
 		slidesToShow: 4,
 		slidesToScroll: 3,
@@ -47,9 +49,12 @@ $(function(){
   	focusOnSelect: true
 	});
 
+	/* Each time a variant is changed, check for a variant with a matching 'data-varientName' to the variant's 'val()'' */
 	var $selector = $('.single-option-selector');
 	$selector.on('change', function(){
-		var $target = $('.product-single__thumbnails a[data-variantName="' + $(this).val() +'"]:first');
+		var $thumbnailContainer = $('.product-single__thumbnails'),
+				$activeVariant = $(this).val(),
+				$target = $thumbnailContainer.find('a[data-variantName="' + $activeVariant +'"]:first');
 		var $index = $target.data('index');
 		if ($index != undefined){
 			$carouselProduct.slick('slickGoTo', $index);
