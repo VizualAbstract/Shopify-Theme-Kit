77fc9b815611046c6daccb2bf87a71f3edf7fa74
Corey Capetillo <corey.capetillo@gmail.com>
2015-07-31 11:07:25 -0700

    Submenu drawer

--- source/javascript/02 custom/01 - main.js
+++ source/javascript/02 custom/01 - main.js
@@ -9,4 +9,19 @@ jQuery(function($) {
 
 	$('.selector-wrapper select').dropkick();
 
+	var $subMenus = $('.js-drawer-open-center');
+
+	/* When a menu with a sub-menu is selected, swap out the available sub-link list with the desired one, changing drawer__title as necessary */
+	$('.js-drawer-open-center').click(function(){
+		// var $subMenu = $('#subMenu'),
+		// 		$target = $(this).data('menu');
+		// var $menu = $subMenu.find('#'+$target);
+		// var $menuTitle = $menu.data('title');
+		// $subMenu.find('ul').each(function(){
+		// 	$(this).removeClass('active');
+		// });
+		// $subMenu.find('.drawer__title').html($menuTitle);
+		// $menu.addClass('active');
+	});
+
 });
